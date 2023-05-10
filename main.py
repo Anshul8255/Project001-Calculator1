d696a508b5e87f22522442efd355c3603bd6a637
Anshul8255 <133140110+Anshul8255@users.noreply.github.com>
2023-05-10 21:44:53 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,15 @@
+
+print("CALCULATOR")
+num1 = int(input('Number 1 : '))
+print(" ")
+opr = input('Operator : ')
+print(" ")
+num2 = int(input('Number 2 : '))
+if opr == '*':
+    print(num1 * num2)
+elif opr == '+':
+    print(num1 + num2)
+elif opr == '/':
+    print(num1 / num2)
+elif opr == '-':
+    print(num1 - num2)
